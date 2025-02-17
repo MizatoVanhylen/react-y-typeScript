02e8a726674cd7ee40609fd1eab22da22ba6e10c
MizatoVanhylen <denisse.romero.c@gmail.com>
2025-02-17 18:03:15 -0300

    se inicia con if, else

---
+++ if-y-swich.js
@@ -0,0 +1,34 @@
+/*const numero = 55;
+
+if (numero > 0 && numero < 50) {
+  console.log("numero es mayor que 0 y menor que 50");
+  if (numero === 5) {
+    console.log("numero es igual a 5");
+  }
+} else {
+  console.log("el numero no es valido")
+}*/
+
+/*const edad = 10;
+
+if (edad >= 18){
+  console.log("puede votar");
+} else if (edad <= 0 || edad > 120){
+  console.log("edad incorrecta");
+  } else {
+  console.log("eres menor de edad")
+    };*/
+
+const color = "rojo";
+
+switch(color) {
+  case "amarillo":
+    console.log ("el color es amarillo");
+    break;
+  case "rojo":
+    console.log ("el color es rojo");
+    break;
+  default:
+    console.log("el color no es amarillo ni rojo");
+  break;
+}
---
+++ ternario.js
@@ -0,0 +1,28 @@
+/*const estatus = "pendiente";
+const pagado = true;
+
+if (estatus === "Completado" && pagado === true){
+  console.log("el pedido esta completado y pagado");
+}/* else if ( estatus === "pendiente" && pagado === true){
+  console.log ("el pedido esta en camino");
+} else if (estatus === null || pagado === null){
+  console.log("el pedido no se ha creado");
+}else if (estatus !== null && pagado !== null){
+  console.log("el pedido esta pendiente de pago");
+}*/
+
+/*const nombre = null;
+const nombreDefecto = "invitado";
+
+const nombreUsuario = nombre ?? nombreDefecto;
+
+console.log(nombreUsuario);*/
+
+const hora = 14
+const saludo = hora < 12 ? "buenos días" : "Buenas tardes";
+console.log(saludo);
+
+
+
+/* AND &&, OR ||, NOT !, NULLISH COALASCING ?? */
+/* TERNARIO (CONDICION ? TRUE : FALSE)*/
